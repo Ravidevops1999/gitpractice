728132a44b7aa5a0c18874b166e9ffba3d4c8e88
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2024-08-27 23:21:56 +0530

    package script new line added

--- installing package.sh
+++ installing package.sh
@@ -34,5 +34,6 @@ do
         dnf install $package -y
         VALIDATE $? "Installing $package"
     else
-        echo " $package is already installed..nothing to do" 
+        echo " $package is already installed..nothing to do"
+    fi     
 done
