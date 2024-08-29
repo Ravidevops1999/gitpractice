f364d8ce94b9a2542cf8a18e7881ca5530fd5e5c
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2024-08-29 23:21:59 +0530

    validation function

---
+++ validate_function.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo " please login with root user..."
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is failed.."
+    else
+        echo "$2 is success.."    
+}
+
+dnf list installed git -y
+
+if [ $? -ne 0 ]
+then
+    echo "git is not installed going to install it ..."
+    dnf install git -y
+   VALIDATE $? "git installation"
+else
+    echo " git is already installed..."
+fi
+
+dnf list installed mysql -y
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed going to install it..."
+    dnf install mysql -y
+    VALIDATE $? "mysql installation"
+else
+    echo " mysql installation is already installed"
+fi                
+    
