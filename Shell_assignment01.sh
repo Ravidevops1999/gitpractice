12bbd909db6dff1eb688488722ac31dd8d138939
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2024-08-31 21:01:02 +0530

    new script

--- Shell_assignment01.sh
+++ Shell_assignment01.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
-file="/var/shell/output.txt"
-while read line; 
+ #!/bin/bash
+# file="/var/shell/output.txt"
+# while read line; 
+# do
+#     for word in $line; 
+#     do
+#         echo "word = '$word'"
+#     done
+# done <"$file"
+
+sort /var/shell/output.txt | uniq -c | sort -nr | while read count name
 do
-    for word in $line; 
-    do
-        echo "word = '$word'"
-    done
-done <"$file"
+    if [ ${count} -gt 1]
+    then 
+        echo "${name} ${count}"
+    fi
+done
