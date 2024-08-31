2a5fb25b0e610ef588fd391c443d4c4fd3a37cb0
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2024-08-31 22:29:24 +0530

    new lines are added

--- Shell_assignment01.sh
+++ Shell_assignment01.sh
@@ -1,13 +1,26 @@
  !/bin/bash
+# file="/var/shell/output.txt"
+# while read line; 
+# do
+#     for word in $line; 
+#     do
+#         echo "word = '$word'"
+#     done
+    
+# done <"$file"
+
+
 file="/var/shell/output.txt"
 while read line; 
 do
-    for word in $line; 
+    while read count word in $line; 
     do
-        echo "word = '$word'"
-    done
-done <"$file"
+        if [ $count -gt 1 ]
 
+        echo "word = '$word'" | echo "$count"
+    done
+    
+done <"$file"
 
 
 
